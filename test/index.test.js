336aa8113bc231a8af81c6eb8af3238c1a8f13d7
Toshiya Komoda <toshiya.komoda@gmail.com>
2017-08-13 19:02:01 +0900

    fix: debug about create_files format.

--- test/index.test.js
+++ test/index.test.js
@@ -5,36 +5,35 @@ let diffContent
 global.danger = {
   git: {
     created_files: ['./src/index.js'],
-    modified_files: [],
-    diffForFile: (file) => {
+    modified_files: ['./test/index.test.js'],
+    diffForFile: file => {
       return new Promise((resolve, reject) => {
         resolve(diffContent)
       })
-    }
-  }
+    },
+  },
 }
-global.fail = (message) => {
+global.fail = message => {
   failCallCount += 1
 }
 
 describe('wordcheck()', () => {
-  let wordcheckFile = "./test/WORDCHECK.txt" 
+  let wordcheckFile = './test/WORDCHECK.txt'
   beforeEach(() => {
     failCallCount = 0
   })
-  it('fail when words in WORDCHECK.txt included', (done) => {
-    diffContent = "test\n"
-    let promises = wordcheck(wordcheckFile)
-    Promise.all(promises).then(() => {
+
+  it('fail when words in WORDCHECK.txt included', done => {
+    diffContent = { diff: '+FOOBAR\n' }
+    wordcheck(wordcheckFile).then(() => {
       expect(failCallCount).toBe(1)
       done()
     })
   })
 
-  it('pass when words in WORDCHECK.txt are not included', (done) => {
-    diffContent = "+let a=0\n+console.log(a)\n"
-    let promises = wordcheck(wordcheckFile)
-    Promise.all(promises).then(() => {
+  it('pass when words in WORDCHECK.txt are not included', done => {
+    diffContent = { diff: '+let a=0\n+console.log(a)\n-FOOBAR' }
+    wordcheck(wordcheckFile).then(() => {
       expect(failCallCount).toBe(0)
       done()
     })
